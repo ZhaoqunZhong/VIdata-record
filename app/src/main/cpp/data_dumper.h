92bd23bf2c45afb14916b9f9ad1e47b2aed1b318
ZhaoqunZhong <zhongzhaoqun1991@outlook.com>
2021-10-21 15:15:33 +0800

    fix commit error

--- app/src/main/cpp/data_dumper.h
+++ app/src/main/cpp/data_dumper.h
@@ -6,6 +6,7 @@
 #include "cam_publisher.h"
 #include "imu_publisher.h"
 #include <queue>
+#include "RosbagPacker.h"
 
 class DataDumper {
 public:
@@ -28,6 +29,9 @@ private:
     pthread_mutex_t acc_mtx_ = PTHREAD_MUTEX_INITIALIZER, gyr_mtx_ = PTHREAD_MUTEX_INITIALIZER,
     imu_mtx_ = PTHREAD_MUTEX_INITIALIZER;
     pthread_t main_th_;
+
+    /// rosbag
+    RosbagPacker bag_packer_;
 };
 #endif //VID_DATA_DUMPER_H
 
